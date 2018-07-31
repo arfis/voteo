ef5686e03685daa56f9f0741c75b2212b30f599f
Michal Ševčík <sevcikmi@cngroup.dk>
2018-07-31 23:17:25 +0200

    smart contract and frontend usage

--- src/app/shared/localizationDatePipe/localization-date-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LocalizationDatePipePipe } from './localization-date-pipe.pipe';
-
-describe('LocalizationDatePipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new LocalizationDatePipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
